c8025d0d62bdd24dfbdec981ee715c683c02880a
janny <jannycedeno2002@gmail.com>
2025-09-24 10:15:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/examen_final/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.examen_final;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Aplicar márgenes para que el contenido no quede oculto bajo las barras del sistema
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
